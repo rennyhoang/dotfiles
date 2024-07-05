a4f162714db9c77cf553efd1801b688925e1e66e
Renny Hoang <renny@rennyhoang.com>
2024-07-05 11:02:02 -0500

    update dotfiles

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -22,6 +22,8 @@ return {
     event = 'InsertEnter',
     dependencies = {
       {'L3MON4D3/LuaSnip'},
+      {'saadparwaiz1/cmp_luasnip'},
+      { "rafamadriz/friendly-snippets" },
     },
     config = function()
       -- Here is where you configure the autocompletion settings.
@@ -31,8 +33,13 @@ return {
       -- And you can configure cmp even more, if you want to.
       local cmp = require('cmp')
       local cmp_action = lsp_zero.cmp_action()
+      require('luasnip.loaders.from_vscode').lazy_load()
 
       cmp.setup({
+        sources = {
+          {name = 'nvim_lsp'},
+          {name = 'luasnip'},
+        },
         formatting = lsp_zero.cmp_format({details = true}),
         mapping = cmp.mapping.preset.insert({
           ['<C-Space>'] = cmp.mapping.complete(),
